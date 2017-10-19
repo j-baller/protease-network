37230fb2d34be7b992d05f65da00e180001d65d2
Joshua Baller <jballer@umn.edu>
2017-10-19 16:49:55 -0500

    Pulled out peptide comparision into it's own module (compare_peptides), reworking Kmer_analysis.py to support fractional kmer counts.

--- Kmer_analysis.py
+++ Kmer_analysis.py
@@ -5,7 +5,9 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import random
-
+import math
+import compare_peptides #Local Module
+ 
 
 parser = argparse.ArgumentParser(description=\
   'This program accepts fasta or fastq files and analyzes kmer distribution of a fixed size')
@@ -24,26 +26,47 @@ parser.add_argument('-c',action="store",type=float,dest="min_edge",required=Fals
   help="minimal edge weight to keep")
 parser.add_argument('-n',action="store",type=int,dest="min_node",required=False, default=1,\
   help="minimal node weight to keep")
-parser.add_argument('-d',action="store",type=int,dest="min_degree",required=False, default=1,\
+parser.add_argument('-m',action="store",type=int,dest="max_node_count",required=False, default=5000,\
+  help="maximum number of nodes afer enrichment step. Nodes are pruned based on the least kmer count first")
+parser.add_argument('-d',action="store",type=int,dest="min_degree",required=False, default=3,\
   help="minimal node degree to keep")
 parser.add_argument('-e',action="store",type=float,dest="min_enrichment",required=False, default=2,\
   help="Proportional fold enrichment of node weight (after pruning by minimum node weight. Only has an effect if a background distribution is provided.")
 parser.add_argument("-l", "--left_flank", help="Amino Acid sequence of Left Flank", default='PQP')
 parser.add_argument("-r", "--right_flank", help="Amino Acid sequence of Right Flank", default='PQP')
 parser.add_argument("--replace_X", help="replace X's with flanking sequences",action="store_true",dest="replace_flag",default=False)
+parser.add_argument('-x', "--enrichment_only", help="only perform enrichment checking, skip network filtering",action="store_true",dest="enrich_flag",default=False)
+parser.add_argument('-z', "--control_zero_option", help="How to handle cases where the control pool has zeros when determining enrichment",required=False,dest="control_zero",default="average",choices=['average','avg','pseudo'])
+parser.add_argument('-n', '--normalize_off',action="store_false",dest="norm_flag",default=True, \
+  help="Shuts per comparision normalization off, returns a score in cale of actual amino acid matrix")
+parser.add_argument('-a', '--align',action="store_true",dest="align_flag",default=False, \
+  help="Return maximum score obtainable by sliding kmers relative to each other rather than assuming they are already aligned. Edges are padded with Xs")
 cmd_args = parser.parse_args()
 
 #### Development Code Snippets ############
 #nx.connected_component_subgraphs(G) Can be used to get a list of subgraphs in graph.
 
+#Get number of subgraphs
+#[len(c) for c in net.connected_component_subgraphs(e) if len(c) > 10]
+
+#Copy Network with Trimming:
+# def trim_edges(g, weight=1):
+        # g2=net.Graph()
+        # for f, to, edata in g.edges(data=True):
+                # if edata['weight'] > weight:
+                        # g2.add_edge(f,to,edata)
+        # return g2
+
 
 
 ##################################
-#### Hardcoded Values ############
-max_score=cmd_args.kmer_size*17 #based on maximum positive in PAM250 matrix. This is the best possible score,
-min_score=cmd_args.kmer_size*-8
+#### Hardcoded Values ############ #Should no longer be used, derived directly from loaded matrix
+#max_score=cmd_args.kmer_size*17 #based on maximum positive in PAM250 matrix. This is the best possible score,
+#min_score=cmd_args.kmer_size*-8
 # rescaling is then, for score S, edge_weight =(S-min_score)/(max_score-min_score), making a score of 1 == perfect and a score of 0 equal to worst possible.
 ##################################
+input_name = cmd_args.in_filename.split(".")[0]
+
 def replace_Xs(peptide,replace_flag):
 	front_list = [i for i, ltr in enumerate(peptide) if ltr == 'x' and i < len(peptide)/2]
 	back_list = [i for i, ltr in enumerate(peptide) if ltr == 'x' and i > len(peptide)/2]
@@ -53,39 +76,65 @@ def replace_Xs(peptide,replace_flag):
 	else:
 		return [peptide[:len(front_list)], "".join(curr_str), peptide[(len(peptide)-len(back_list)):]]
 
-input_name = cmd_args.in_filename.split(".")[0]
+def prep_kmer():
+	curr_seq = line.rstrip().lower()
+				f_str, mid_str, l_str = replace_Xs(curr_seq, cmd_args.replace_flag)
+				if cmd_args.randomize_flag:
+					mid_str = ''.join(random.sample(mid_str, len(mid_str)))
+				curr_seq = f_str+mid_str+l_str
+				curr_seq = curr_seq.lower()
+				while len(curr_seq) >= cmd_args.kmer_size:
+					master_dict[curr_seq[0:cmd_args.kmer_size]] = master_dict.get(curr_seq[0:cmd_args.kmer_size], 0) + 1
+					curr_seq = curr_seq[1:]
+					total_kmers +=1
+
+def load_sequences(curr_filename):
+	total_kmers = 0
+	in_handle = open(curr_filename, 'r')
+	master_dict = {}
+	if cmd_args.is_fastq: #if is_fastq
+		for line in in_handle:
+			if line_count%4 == 1:
+				prep_kmer()
+		in_handle.close()
+		
+	else: #is_fasta
+		curr_seq = ""
+		for line in in_handle:
+			if line[0] == ">":
+				prep_kmer()
+				curr_seq = ""
+			else:
+				curr_seq = curr_seq + line.rstrip().lower()
+			f_str, mid_str, l_str = replace_Xs(curr_seq, cmd_args.replace_flag)
+			
+			#If random Label Set, generate a randomized sequence set.
+			if cmd_args.randomize_flag:
+				mid_str = ''.join(random.sample(mid_str, len(mid_str)))
+			curr_seq = f_str+mid_str+l_str
+			curr_seq = curr_seq.lower()
+			while len(curr_seq) >= cmd_args.kmer_size:
+						master_dict[curr_seq[0:cmd_args.kmer_size]] = master_dict.get(curr_seq[0:cmd_args.kmer_size], 0) + 1
+						curr_seq = curr_seq[1:]
+						total_kmers +=1
+		in_handle.close()
+	return((master_dict, total_kmers))
+
+def plot_network(t_graph, filename_detail, fig_x=16,fig_y=22):
+	#Prepare plot of kmer network
+	plt.clf()
+	plt.figure(figsize=(fig_x,fig_y))
+	pos = nx.spring_layout(m_graph) 
+
+	#get max node weight
+	node_weights = max(list(nx.get_node_attributes(t_graph,'weight').values()))
 	
-
-class PAM250(object):
-    """The PAM250 scoring matrix class."""
-
-    def __init__(self):
-        """Initialize the scoring matrix."""
-        with open(os.path.join(os.path.dirname(__file__), 'PAM250wX.txt')) as input_data:
-            items = [line.strip().split() for line in input_data.readlines()]
-            self.scoring_matrix = {(item[0], item[1]):int(item[2]) for item in items}
-
-    def __getitem__(self, pair):
-        """Returns the score of the given pair of protein."""
-        return self.scoring_matrix[pair[0], pair[1]]
-        
-class PAM250_special(object):
-	"""The PAM250 scoring matrix class."""
-	def __init__(self):
-		"""Initialize the scoring matrix."""
-		with open(os.path.join(os.path.dirname(__file__), 'PAM250wX.txt')) as input_data:
-			items = [line.strip().split() for line in input_data.readlines()]
-			self.scoring_matrix = {}
-			for item in items:
-				if item[0] in self.scoring_matrix:
-					self.scoring_matrix[item[0]][item[1]] = int(item[2])
-				else:
-					self.scoring_matrix[item[0]] = {item[1]:int(item[2])}
-	def __getitem__(self, pair):
-		"""Returns the score of the given pair of protein."""
-		char_max = max(list(self.scoring_matrix[pair[0]].values())+list(self.scoring_matrix[pair[1]].values())) 
-		char_min = min(list(self.scoring_matrix[pair[0]].values())+list(self.scoring_matrix[pair[1]].values()))
-		return ((self.scoring_matrix[pair[0]][pair[1]]-char_min)/(char_max-char_min))*25-8
+	#Draw Graph Elements
+	nx.draw_networkx_edges(t_graph, pos,width=[math.log(edata['weight']-cmd_args.min_edge+.001)*2.5 for u,v,edata in t_graph.edges(data=True)],alpha=.3,edge_color='m')
+	nx.draw_networkx_nodes(t_graph, pos,with_labels=True,node_size=[ndata['weight']/node_weights*100 for n,ndata in t_graph.nodes(data=True)])
+	nx.draw_networkx_labels(t_graph,pos,fontsize=14)
+	
+	plt.savefig("".join(["network",filename_detail])+".png",format="png")
 
 
 if cmd_args.randomize_flag:
@@ -97,101 +146,83 @@ print("Output Name: "+output_id)
 master_dict = {}
 m_graph = nx.Graph()
 
-def load_sequences(curr_filename):
-	total_kmers = 0
-	in_handle = open(curr_filename, 'r')
-	master_dict = {}
-	if cmd_args.is_fastq: #if is_fastq
-		for line in in_handle:
-			if line_count%4 == 1:
-				curr_seq = line.rstrip().lower()
-				f_str, mid_str, l_str = replace_Xs(curr_seq, cmd_args.replace_flag)
-				if cmd_args.randomize_flag:
-					mid_str = ''.join(random.sample(mid_str, len(mid_str)))
-				curr_seq = f_str+mid_str+l_str
-				while len(curr_seq) >= cmd_args.kmer_size:
-					master_dict[curr_seq[0:cmd_args.kmer_size]] = master_dict.get(curr_seq[0:cmd_args.kmer_size], 0) + 1
-					curr_seq = curr_seq[1:]
-					total_kmers +=1
-		in_handle.close()
-		
-	else: #is_fasta
-		curr_seq = ""
-		for line in in_handle:
-			if line[0] == ">":
-				f_str, mid_str, l_str = replace_Xs(curr_seq, cmd_args.replace_flag)
-				if cmd_args.randomize_flag:
-					mid_str = ''.join(random.sample(mid_str, len(mid_str)))
-				curr_seq = f_str+mid_str+l_str
-				while len(curr_seq) >= cmd_args.kmer_size:
-					master_dict[curr_seq[0:cmd_args.kmer_size]] = master_dict.get(curr_seq[0:cmd_args.kmer_size], 0) + 1
-					curr_seq = curr_seq[1:]
-					total_kmers +=1
-				curr_seq = ""
-			else:
-				curr_seq = curr_seq + line.rstrip().lower()
-		f_str, mid_str, l_str = replace_Xs(curr_seq, cmd_args.replace_flag)
-		if cmd_args.randomize_flag:
-			mid_str = ''.join(random.sample(mid_str, len(mid_str)))
-		curr_seq = f_str+mid_str+l_str
-		while len(curr_seq) >= cmd_args.kmer_size:
-					master_dict[curr_seq[0:cmd_args.kmer_size]] = master_dict.get(curr_seq[0:cmd_args.kmer_size], 0) + 1
-					curr_seq = curr_seq[1:]
-					total_kmers +=1
-		in_handle.close()
-	return((master_dict, total_kmers))
-	
+#Define Distance Matrix for comparing strings.
+score_mat = PAM250_special()
+pd = scoring_distance(cmd_args.norm_flag)
+
 print("Input path resolved to:", os.path.abspath(cmd_args.in_filename))
 main_dict, total_main = load_sequences(os.path.abspath(cmd_args.in_filename))
 
-if cmd_args.in_background != "":
-	print("Input path resolved to:", os.path.abspath(cmd_args.in_background))
-	background_dict, total_back = load_sequences(os.path.abspath(cmd_args.in_background))
-
-#Define Distance Matrix for comparing strings.
-score_mat = PAM250_special() 
-
+#Convert Primary Dictionary to Node_set, filter out kmers with fewer than a minimum occurance cutoff
 node_set = [(i,{'weight':j}) for i,j in main_dict.items() if j >= cmd_args.min_node]
 
 #Create Nodes in Network with a weight characteristic
 m_graph.add_nodes_from(node_set)
 node_weights = nx.get_node_attributes(m_graph,'weight').values()
 
-#If background is defined, check enrichment levels
+#Print plot showing kmer_distribution
+fig=plt.hist([math.log10(x) for x in list(node_weights)])
+#fig = plt.gcf()
+plt.savefig("kmer_frequency_histo"+output_id+".png",format="png")
+
+#If background is defined load the data, check enrichment levels
 #Remove all nodes that have an enrichment lower than allowable enrichment.
 if cmd_args.in_background != "":
+	print("Input path resolved to:", os.path.abspath(cmd_args.in_background))
+	background_dict, total_back = load_sequences(os.path.abspath(cmd_args.in_background))
+
 	back_set = [(i,{'weight':j}) for i,j in background_dict.items()]
 	b_graph = nx.Graph()
 	b_graph.add_nodes_from(back_set)
 	
 	remove_list = []
+	enrich_attribute_dict = {}
 	for n in m_graph:
-		if n in b_graph:
-			#print((m_graph.node[n]["weight"]),(b_graph.node[n]["weight"]))
-			if (m_graph.node[n]["weight"]/total_main)/(b_graph.node[n]["weight"]/total_back) < cmd_args.min_enrichment:
+		if cmd_args.control_zero in ['average','avg']:
+			if n in b_graph:		
+				#Filter on enrichment of each kmer node. Enrichment is the proportional occurance in the main set, divided by the proportional occurance in the background set.
+				temp_enrich = (m_graph.node[n]["weight"]/total_main)/(b_graph.node[n]["weight"]/total_back)
+				if temp_enrich < cmd_args.min_enrichment:
+					remove_list.append(n)
+				else:
+					enrich_attribute_dict[n] = temp_enrich
+			else:
+				#Set background to average Kmer proportion
+				temp_enrich = (m_graph.node[n]["weight"]/total_main)/(1.0/len(back_set))
+				if temp_enrich < cmd_args.min_enrichment:
+					remove_list.append(n)
+				else:
+					enrich_attribute_dict[n] = temp_enrich
+		elif cmd_args.control_zero in ['pseudo']:
+			#Filter on enrichment of each kmer node. Enrichment is the proportional occurance in the main set, divided by the proportional occurance in the background set. Adding Pseudocount of 1 to the background and foreground
+			temp_enrich = ((m_graph.node.get(n,{'weight':0})["weight"]+1)/total_main)/((b_graph.node.get(n,{'weight':0})["weight"]+1)/total_back)
+			if temp_enrich < cmd_args.min_enrichment:
 				remove_list.append(n)
-		else: # if not in background, assume present at average rate
-			#print(n,"not in background, substituting average occurance")
-			if (m_graph.node[n]["weight"]/total_main)/(1.0/len(back_set)) < cmd_args.min_enrichment:
-				remove_list.append(n)
+			else:
+				enrich_attribute_dict[n] = temp_enrich
+			
 	m_graph.remove_nodes_from(remove_list)
-				
+	#Add enrich attribute to nodes
+	nx.set_node_attributes(m_graph, 'enrich', enrich_attribute_dict)			
 del back_set			
 	
 
 past_set = []
-fig=plt.hist(list(nx.get_node_attributes(m_graph,'weight').values()))
-#fig = plt.gcf()
-plt.savefig("kmer_frequency_histo"+output_id+".png",format="png")
 
-print("Total Number of Nodes", str(len(m_graph.node)))
+
+
+print("Total Number of filtered Nodes", str(len(m_graph.node)))
 
 #Compute distances between nodes
 for i in m_graph.nodes_iter(): 
 	for j in past_set:
 		#print(i,j,[score_mat[i[c].upper(),j[c].upper()] for c in range(cmd_args.kmer_size)])
 		#Distance calculation between nodes
-		edge_weight = (sum((score_mat[i[c].upper(),j[c].upper()] for c in range(cmd_args.kmer_size)))-min_score)/(max_score-min_score)
+		if cmd_args.align_flag:
+			edge_weight = pd.compare_peptides_mp(i,j)
+		else:
+			edge_weight = pd.compare_peptides_sp(i,j)
+		#edge_weight = (sum((score_mat[i[c].upper(),j[c].upper()] for c in range(cmd_args.kmer_size)))-min_score)/(max_score-min_score) #Old edge weight code, now in external module
 		if edge_weight >= cmd_args.min_edge:
 			m_graph.add_edge(i,j,weight=edge_weight) #Insert Edges if greater than minimum
 		
@@ -208,22 +239,23 @@ if cmd_args.in_background != "":
 			for i in range(node_weight[n]):
 				print(n,file=del_out)
 
+#Delete nodes failing degree criteria
 m_graph.remove_nodes_from([n for n,d in m_graph.degree_iter() if d < cmd_args.min_degree])
-node_weights = list(nx.get_node_attributes(b_graph,'weight').values())
 
-plt.clf()
-plt.figure(figsize=(16,22))
-pos = nx.spring_layout(m_graph) 
+#Plot Graph ####
+plot_network(m_graph, output_id)
 
-nx.draw_networkx_edges(m_graph, pos,width=[edata['weight']*edata['weight']*10 for u,v,edata in m_graph.edges(data=True)],alpha=.3,edge_color='m')
-nx.draw_networkx_nodes(m_graph, pos,with_labels=True,node_size=[ndata['weight']/max(node_weights)*100 for n,ndata in m_graph.nodes(data=True)])
-nx.draw_networkx_labels(m_graph,pos,fontsize=14)
-plt.savefig("".join(["network",output_id])+".png",format="png")
-
+#Output Kmers that pass all criteria, replicated for the number of times they occur (WebLogo input)
 kmer_out = open("kmer_output"+input_name+"_"+output_id+".txt", 'w')
 for n,ndata in m_graph.nodes(data=True):
 	for i in range(ndata['weight']):
 		print(n,file=kmer_out)
+kmer_out.close()
+
+#Output information about all Kmer nodes that pass criteria
+kmer_out = open("node_info"+input_name+"_"+output_id+".txt", 'w')
+for n,ndata in m_graph.nodes(data=True):
+	print(n+"\t"+str(ndata['weight'])+"\t"+str(ndata['enrich'])+"\t"+str(m_graph.degree(n))+"\t"+str(m_graph.degree(n,weight="weight")),file=kmer_out)
 kmer_out.close()
 
 #print(m_graph.edges(data=True))
