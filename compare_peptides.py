1d3299466dcbe07a3d8f3ed5de99bc9a5e019811
jballer <jballer@umn.edu>
2017-12-07 13:49:34 -0600

    Update compare_peptides.py
    First stage of addition of PWM handling

--- compare_peptides.py
+++ compare_peptides.py
@@ -1,6 +1,46 @@
 import os;
 import math;
-   
+
+class PWM:
+	__init__(self,in_str,score_obj):
+		self._curr_PWM = [Counter(char) for char in in_str]
+		self.score_obj = score_obj
+		self.coherance = 0
+	def length(self)
+		return len(self._curr_PWM)
+	
+	def _compare_PWM_sp(self, pwm1, pwm2, phase=0):
+		idx1 = range(0,pwm1.length())
+		idx2 = range(0,pwm2.length())
+		if phase > 0:
+			idx2 = [-1]*phase + idx2
+		elif phase < 0:
+			idx1 = [-1]*abs(phase) + idx1
+		if len(pep1) > len(pep2):
+			idx2 = idx2 + [-1]*(len(idx1) - len(idx2))
+		elif len(pep2) > len(pep1):
+			idx1 = idx1 + [-1]*(len(idx2) - len(idx1))
+		n_iter = len(idx1) #equal to len(pep2) due to 'X' padding
+		
+		#Still needs work below here
+		align_score =0
+		for p in range(n_iter):
+			if idx1[p] == -1 and indx2[p] == -1:
+				
+		edge_weight = (sum((self[pep1[c].upper(),pep2[c].upper()]-self.min_score)/(self.max_score-self.min_score) for c in range(n_iter)))/n_iter
+
+		return(edge_weight)
+		
+	def __add__(self, other):
+		if type(other) == str:
+			self+PWM(str, self.score_obj)
+		elif type(other) == PWM:
+			if self.score_obj != other.score_obj:
+				raise TypeError('Addition PWMs is only defined for PWMs using the same scoring object')
+			self._compare_PWM_mp(self.score_obj, other.score_obj)
+		else:
+			raise TypeError('Addition of PWMs is only defined for Strings and PWMs')
+		
 class scoring_distance(object):
 	"""The PAM250 scoring matrix class."""
 	def __init__(self, normalize = False, debug=False):
@@ -16,7 +56,7 @@ class scoring_distance(object):
 		self.max_score = max(max(self.scoring_matrix.values(), key=lambda v: max(v.values())).values())
 		self.min_score = min(min(self.scoring_matrix.values(), key=lambda v: min(v.values())).values())
 		self.per_pos_norm = normalize
-		self.debug=debug
+		self._debug=debug
 	def __getitem__(self, pair):
 		"""Returns the score of the given pair of AA."""
 		if self.per_pos_norm==True:
@@ -37,19 +77,23 @@ class scoring_distance(object):
 			pep1 = pep1 + 'X'*(len(pep2) - len(pep1))
 		n_iter = len(pep1) #equal to len(pep2) due to 'X' padding
 		edge_weight = (sum((self[pep1[c].upper(),pep2[c].upper()]-self.min_score)/(self.max_score-self.min_score) for c in range(n_iter)))/n_iter
-		if self.debug: print(pep1.upper(), pep2.upper(), n_iter)
-		if self.debug: print(edge_weight)
+		if self._debug: print(pep1.upper(), pep2.upper(), n_iter)
+		if self._debug: print(edge_weight)
 		return(edge_weight)
 	
 	def compare_peptides_mp(self,pep1, pep2):
+		return(self._compare_peptide_core_mp(pep1,pep2)[0])
+	
+	def _compare_peptide_core_mp(self,pep1,pep2):
 		phases = len(pep1)+len(pep2)-1
 		min_phase = -1*len(pep2)+1
 		out = []
 		#Total phases are (n+m)-1, 
 		for p in range(min_phase, phases+min_phase):
 			out.append(self.compare_peptides_sp(pep1,pep2,phase=p))
-		if self.debug: print(list(zip(range(min_phase, phases+min_phase), out)))
-		return(max(out))
+		if self._debug: print(list(zip(range(min_phase, phases+min_phase), out)))
+		return(max(out), [min_phase+i for i, j in enumerate(out) if j == max(out)])
+	def
 			 
 		
 if __name__ == "__main__":
@@ -68,7 +112,7 @@ if __name__ == "__main__":
   help="Prints debug information")   
 	cmd_args = parser.parse_args()
 	
-	pd = scoring_distance(cmd_args.norm_flag, cmd_args.debug_flag)
+	pd = scoring_distance(cmd_args.norm_flag, cmd_args._debug_flag)
 
 	if cmd_args.align_flag:
 		print(pd.compare_peptides_mp(cmd_args.peptide_1,cmd_args.peptide_2))
