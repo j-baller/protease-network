2a793255b5fa708d5155e8fa28b45494aa585005
Joshua Baller <jballer@umn.edu>
2020-03-14 12:54:35 -0500

    Update do_kmer_analysis.sh

--- test_scripts/do_kmer_analysis.sh
+++ test_scripts/do_kmer_analysis.sh
@@ -10,7 +10,7 @@ kmer_set()
 	e=$5
 	d=$6
 	c=$7
-	python3 ../Kmer_analysis.py $casefile -b $controlfile -k $k -n $n -e $e -d $d -c $c -a -z "pseudo" --replace_X --debug -p 'even' 
+	python3 ../Kmer_analysis.py $casefile -b $controlfile -k $k -n $n -e $e -d $d -c $c -a -z "pseudo" --replace_X --debug -p "even" 
 	python3 ../Kmer_analysis.py $casefile -b $controlfile -k $k -n $n -e $e -d $d -c $c --random --replace_X &
 }
 # Input, Background, kmer_size, minimum_count, fold_enrichment, min_degree, similarity_cutoff 
